10c2e92322442f0c56589cbba890b08ab67ceec8
cnewton <christnewton@gmail.com>
2019-06-05 08:32:57 -0700

    checkin

---
+++ cpap_sounder.py
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+#
+use strict;
+
+print "kicking off after reboot:\n";
+
+while (1)
+{
+  system ("AUDIODEV=hw:1 rec file_size.wav trim 0 6 2>/dev/null ");
+  system ("gzip -f file_size.wav");
+  my $size = `stat -c %s file_size.wav.gz`;
+  chop $size;
+  print "$size\n";
+  # 10 minute file size on the low end when no sound(cpap going): 
+  # 4217427
+  if ( $size < 47612996 )
+  {
+    system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+  }
+}
