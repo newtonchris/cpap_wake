f2667deebebbf203bd4f450be822709f56e0a8b3
cnewton <christnewton@gmail.com>
2020-01-02 22:38:57 -0800

    checkin

--- cpap_sounder.pl
+++ cpap_sounder.pl
@@ -11,61 +11,62 @@ print "sleeping for 15 minutes before starting if this is the first start of the
 my $find_count = `find /var/log/cpap_sounder.log -cmin -110 -ls| wc -l`;
 if ( $find_count eq 1 )
 {
-  # only sleep if this is the first reboot of the night
-  system ("sleep 900");
+  # sleep for 1 minute to minimize chances of false alarm
+  system ("sleep 60");
 }
+else
 {
-  # sleep for 2 minutes to minimize chances of false alarm
-  system ("sleep 120");
+  # only sleep if this is the first reboot of the night
+  system ("sleep 5400");
 }
-
-  # 10 minute file size on the low end when no sound(cpap going): 
-  # 4217427
-  # readings where the device was off my head but still pumping air(the ones that are over 6):
-  # Filesize at Sat Aug 31 02:22:30 PDT 2019 : 51720078
-  # Filesize at Sat Aug 31 02:33:00 PDT 2019 : 54556172
-  # Filesize at Sat Aug 31 02:43:31 PDT 2019 : 61970413
-  # Filesize at Sat Aug 31 02:54:01 PDT 2019 : 62153278
-  # Filesize at Sat Aug 31 03:04:29 PDT 2019 : 63301149
-  # Filesize at Sat Aug 31 03:14:58 PDT 2019 : 62941282
-  # Filesize at Sat Aug 31 03:25:31 PDT 2019 : 62030976
-  # Filesize at Sat Aug 31 03:36:00 PDT 2019 : 62584369
-  # Filesize at Sat Aug 31 03:46:29 PDT 2019 : 62722421
-  # Filesize at Sat Aug 31 03:57:03 PDT 2019 : 62665817
-  # Filesize at Sat Aug 31 04:07:32 PDT 2019 : 62750951
-  # Filesize at Sat Aug 31 04:18:01 PDT 2019 : 62739306
-  # Filesize at Sat Aug 31 04:28:32 PDT 2019 : 62746465
-  # Filesize at Sat Aug 31 04:39:05 PDT 2019 : 62777177
-  # Filesize at Sat Aug 31 04:49:34 PDT 2019 : 62936930
-  # Filesize at Sat Aug 31 05:00:03 PDT 2019 : 62506056
-  # Filesize at Sat Aug 31 05:10:34 PDT 2019 : 55830901
-  
 
 while (1)
 {
   system ("AUDIODEV=hw:1 rec file_size.wav trim 0 600 2>/dev/null ");
   system ("gzip --best -f file_size.wav");
+  system ("ls -alt /home/pi/sound_in/file_size.wav.gz");
   my $size = `stat -c %s file_size.wav.gz`;
   chop $size;
   $date = `date`;
   chop $date;
   print "Filesize at $date : $size\n";
   if ( $size < 42612996 )  
+  #if ( $size < 25306498 )
   {
     $date = `date`;
     chop $date;
     print "Playing song at $date size : $size\n";
-    system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+    system ("AUDIODEV=hw:2 play bigmikeydread_2013-05-27T11_25_52-07_00.mp3& > /dev/null 2>&1");
+    print "Playing song at $date size : $size\n";
+    system ("sleep 30");
+    system ("pkill -9 play");
+    #system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+    #print "Playing song at $date size : $size\n";
+    #system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+    #print "Playing song at $date size : $size\n";
+    #system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+    #system ("sleep 90");
+    #system ("pkill -9 play");
     print "Rebooting now at: $date size : $size to verify speaker is working\n";
-    system ("pkill -9 play; sleep 120; shutdown -rf now");
+    system ("pkill -9 play; sleep 12; shutdown -rf now");
   }
-  elsif ( $size > 60000000 )  
+  elsif ( $size > 9960000000 )  
+  #elsif ( $size > 4980000000 )  
   {
     $date = `date`;
     chop $date;
     print "Possibly off head but still pumping air: $date size : $size\n";
-    system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+    print "Playing song at $date size : $size\n";
+    system ("AUDIODEV=hw:2 play bigmikeydread_2013-05-27T11_25_52-07_00.mp3& > /dev/null 2>&1");
+    print "Playing song at $date size : $size\n";
+    system ("sleep 30");
+    system ("pkill -9 play");
+    #system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+    #print "Playing song at $date size : $size\n";
+    #system ("AUDIODEV=hw:2 play Haydn_Adagio.mp3 > /dev/null 2>&1");
+    #system ("sleep 60");
+    #system ("pkill -9 play");
     print "Rebooting now at: $date size : $size to verify speaker is working\n";
-    system ("pkill -9 play; sleep 120; shutdown -rf now");
+    system ("pkill -9 play; sleep 12; shutdown -rf now");
   }
 }
