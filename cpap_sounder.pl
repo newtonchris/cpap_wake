c312d9a16ee7d2f76f3e0d2cac6b072150a99054
cnewton <christnewton@gmail.com>
2020-01-08 10:48:19 -0800

    fix

--- cpap_sounder.pl
+++ cpap_sounder.pl
@@ -7,12 +7,19 @@ my $date = `date`;
 chop $date;
 
 # number of files older than 120 minutes
+my $ls1 = `ls -lt /var/log/cpap_sounder.touch`;
+chop $ls1;
+my $date1 = `date`;
+chop $date1;
 my $find_count = `find /var/log/cpap_sounder.touch -cmin +120 -ls | wc -l`;
-print "\n\nkicking off after reboot at $date :\n";
-print "find_count = >$find_count<\n";
+chop $find_count;
 # replace any non digit with ""
 $find_count =~ s/\D//g;
-print "find_count = >$find_count<\n";
+
+
+print "\n\nkicking off after reboot at $date1 :\n";
+print ">$ls1<\n";
+print ">$find_count<\n\n";
 if ( $find_count < 1 )
 {
   # sleep for 1 minute to minimize chances of false alarm
@@ -31,9 +38,9 @@ else
 while (1)
 {
   system ("touch /var/log/cpap_sounder.touch");
-  system ("AUDIODEV=hw:1 rec file_size.wav trim 0 600 2>/dev/null ");
+  system ("AUDIODEV=hw:1 rec file_size.wav trim 0 600 > /dev/null 2>&1 ");
   system ("gzip --best -f file_size.wav");
-  system ("ls -lh /home/pi/sound_in/file_size.wav.gz");
+  #system ("ls -l /home/pi/sound_in/file_size.wav.gz");
   my $size = `stat -c %s file_size.wav.gz`;
   chop $size;
   $date = `date`;
@@ -44,21 +51,7 @@ while (1)
   {
     $date = `date`;
     chop $date;
-    print "Playing song at $date size : $size\n";
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("sleep 3");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("sleep 3");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("sleep 3");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("sleep 3");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("sleep 3");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    print "Playing song at $date size : $size\n";
-    system ("sleep 30");
-    system ("pkill -9 play");
+    play_song(10,60);
     print "Rebooting now at: $date size : $size to verify speaker is working\n";
     system ("touch /var/log/cpap_sounder.touch; pkill -9 play; sleep 12; shutdown -rf now");
   }
@@ -67,19 +60,23 @@ while (1)
     $date = `date`;
     chop $date;
     print "Possibly off head but still pumping air: $date size : $size\n";
-    print "Playing song at $date size : $size\n";
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("sleep 30");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    system ("sleep 30");
-    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
-    print "Playing song at $date size : $size\n";
-    system ("sleep 30");
-    system ("pkill -9 play");
+    play_song(10,60);
     print "Rebooting now at: $date size : $size to verify speaker is working\n";
     system ("touch /var/log/cpap_sounder.touch; pkill -9 play; sleep 12; shutdown -rf now");
+  }
+}
+
+sub play_song
+{
+  my ($times_to_play,$seconds_between_play) = @_;
+  my $count = 1;
+  while ($count < $times_to_play)
+  {
+    my $date = `date`;
+    chop $date;
+    system ("AUDIODEV=hw:2 play alarm1.mp3 > /dev/null 2>&1");
+    print "Playing song time # $count at $date\n";
+    system ("sleep $seconds_between_play");
+    $count++;
   }
 }
